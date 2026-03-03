51ef91646a084ad73e2998d44e556ba2bcbed84c
ISpeed2 <bro228nur@gmail.com>
2026-03-03 12:26:44 +0300

    Update Plotly Express.py

--- Plotly Express.py
+++ Plotly Express.py
@@ -6,6 +6,5 @@ df_iris = px.data.iris()
 # 2. Создаем график
 fig = px.scatter(df_iris, x="sepal_width", y="sepal_length", color="species", title="Forced Render")
 
-# 3. ПРИНУДИТЕЛЬНОЕ ОТКРЫТИЕ:
-# Код создаст файл 'temp_plot.html' в папке с вашим скриптом и сам откроет его в браузере
 fig.write_html("temp_plot.html", auto_open=True)
+
