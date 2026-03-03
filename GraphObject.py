cfac86b26dccf58aff7f0f8dedb4b2d879506e53
ISpeed2 <bro228nur@gmail.com>
2026-03-03 12:24:47 +0300

    Add files via upload

---
+++ GraphObject.py
@@ -0,0 +1,12 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# --- 2. Пример для Graph_Objects (Сложный график) ---
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x=[1, 2, 3], y=[4, 1, 2], name="Линия"))
+fig2.add_trace(go.Bar(x=[1, 2, 3], y=[2, 5, 3], name="Столбцы"))
+fig2.update_layout(title="Graph Objects Demo")
+
+# Принудительно сохраняем и открываем первый график
+fig2.write_html("graph_objects.html", auto_open=True)
+
